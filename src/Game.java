27acba9c1ab880023228a108d96ab0ab903d27b7
albertkowalski <albertkowalski@gmail.com>
2021-02-16 17:28:59 +0100

    modified getMovie() method

--- src/Game.java
+++ src/Game.java
@@ -1,29 +1,34 @@
 // Created by Albert Kowalski
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Game {
     private static int guessLimit = 10;
     String title;
 
-    public void getMovie() throws Exception {
+    public void getMovie()  {
         File file = new File("src/movies.txt");
-        Scanner scanner = new Scanner(file);
+        try {
+            Scanner scanner = new Scanner(file);
+            int lines = 0;
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                lines += 1;
+            }
+            int movieIndex = (int) (Math.random() * lines) +1;
+            scanner = new Scanner(file);
+            for (int i=0; i < movieIndex; i++) {
+                title = scanner.nextLine();
+            }
+            System.out.println(title);
+        } catch(FileNotFoundException exception) {
+            System.out.println("Cannot find that file");
+        }
 
-        int lines = 0;
-        while(scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            lines += 1;
-        }
-        int movieIndex = (int) (Math.random() * lines) +1;
-        scanner = new Scanner(file);
-        for (int i=0; i < movieIndex; i++) {
-            title = scanner.nextLine();
-        }
-        System.out.println(title);
     }
-    public Game() throws Exception {
+    public Game() {
         getMovie();
     }
 
@@ -44,7 +49,7 @@ public class Game {
 
 
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Game game = new Game();
         System.out.println((game.title));
         System.out.println(underscore(game.title));
